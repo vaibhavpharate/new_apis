eea0738aa585efee030e0f9d539d6e9cec25189a
vaibhavpharate <62934219+vaibhavpharate@users.noreply.github.com>
2023-12-03 17:16:12 +0530

    Update settings.py

--- new_api_view/settings.py
+++ new_api_view/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = ''
+STATIC_ROOT = 'app-root/repo/wsgi/static'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
